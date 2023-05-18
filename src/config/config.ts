b3cc1ac3a71624f69f68db36bd84b98dbbbef7a0
ppluuums-jp <104049111+ppluuums-jp@users.noreply.github.com>
2023-05-18 22:58:32 +0900

    add: config

---
+++ src/config/config.ts
@@ -0,0 +1,27 @@
+/**
+ * Environment variables
+ * @field
+ * sheetName: Store sheet name
+ * targetSSIds: Target spread sheet ids
+ * emails: Group teacher's email addresses
+ * threshold: Minutes
+ * slackIds: Slack ids
+ * webHookUrl: Slack webhook url
+ */
+const config: Config = {
+  sheetName: PropertiesService.getScriptProperties().getProperty('SHEET_NAME') || 'Sheet1',
+  targetSSIds: JSON.parse(PropertiesService.getScriptProperties().getProperty('TARGET_SPREADSHEET_IDS')) || [],
+  emails: JSON.parse(PropertiesService.getScriptProperties().getProperty('EMAILS')) || [],
+  threshold: parseInt(PropertiesService.getScriptProperties().getProperty('THRESHOLD'), 10) || undefined,
+  slackIds: JSON.parse(PropertiesService.getScriptProperties().getProperty('SLACK_IDS')) || [],
+  webHookUrl: PropertiesService.getScriptProperties().getProperty('WEBHOOK_URL') || '',
+};
+
+type Config = {
+  sheetName: string;
+  targetSSIds: string[];
+  emails: string[];
+  threshold: number;
+  slackIds: string[];
+  webHookUrl: string;
+};
